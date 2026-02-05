6dd1db50910ba4f2eaeaf55a6c4120a5ed3aac78
Anderson <anderg029610@outlook.com>
2026-02-05 17:03:51 -0500

    Agregando telescope

---
+++ lua/plugins/showkeys.lua
@@ -0,0 +1,33 @@
+-- Muestra en pantalla las combinaciones de teclas presionadas en tiempo real.
+-- Ideal para practicar, grabar o depurar tus atajos de Neovim.
+return {
+	"nvzone/showkeys",
+	lazy = false, -- cargar al inicio
+	cmd = "ShowkeysToggle",
+	keys = {
+		{
+			"<leader>ks",
+			"<cmd>ShowkeysToggle<cr>",
+			desc = "Mostrar teclas presionadas (Showkeys)",
+		},
+	},
+	opts = {
+		timeout = 100, -- tiempo de espera para mostrar la combinación
+		maxkeys = 4,
+		position = "bottom-center", -- posición del recuadro
+		winopts = {
+			height = 1, -- altura del recuadro flotante
+		},
+	},
+	config = function()
+		require("showkeys").setup({
+			timeout = 100,
+			maxkeys = 5,
+			position = "bottom-center",
+			winopts = {
+				height = 1,
+			},
+		})
+		vim.cmd("ShowkeysToggle") -- activarlo automáticamente
+	end,
+}
