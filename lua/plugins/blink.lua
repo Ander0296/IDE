1a5c185af76a78e47ad8a0eb692d4c2c1d39765c
Anderson <anderg029610@outlook.com>
2026-02-07 12:20:53 -0500

    Agregando funciones y plugins interesantes

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,76 +1,75 @@
-
 return {
-  -- Snippets (colección)
-  { "rafamadriz/friendly-snippets" },
+	-- Snippets (colección)
+	{ "rafamadriz/friendly-snippets" },
 
-  -- Compat layer (solo si lo necesitas para fuentes estilo nvim-cmp)
-  {
-    "saghen/blink.compat",
-    optional = true,
-    version = "*",
-    opts = {},
-  },
+	-- Compat layer (solo si lo necesitas para fuentes estilo nvim-cmp)
+	{
+		"saghen/blink.compat",
+		optional = true,
+		version = "*",
+		opts = {},
+	},
 
-  -- Completion
-  {
-    "saghen/blink.cmp",
-    version = "*",
-    event = { "InsertEnter", "CmdlineEnter" },
-    dependencies = { "rafamadriz/friendly-snippets" },
+	-- Completion
+	{
+		"saghen/blink.cmp",
+		version = "*",
+		event = { "InsertEnter", "CmdlineEnter" },
+		dependencies = { "rafamadriz/friendly-snippets" },
 
-    opts = {
-      snippets = {
-        preset = "luasnip", -- usa vim.snippet
-      },
+		opts = {
+			snippets = {
+				preset = "luasnip", -- usa vim.snippet
+			},
 
-      appearance = {
-        use_nvim_cmp_as_default = false,
-        nerd_font_variant = "mono",
-      },
+			appearance = {
+				use_nvim_cmp_as_default = false,
+				nerd_font_variant = "mono",
+			},
 
-      completion = {
-        accept = {
-          auto_brackets = { enabled = true },
-        },
-        documentation = {
-          auto_show = true,
-          auto_show_delay_ms = 200,
-        },
-        ghost_text = {
-          enabled = false, -- pon true si quieres “texto fantasma”
-        },
-      },
+			completion = {
+				accept = {
+					auto_brackets = { enabled = true },
+				},
+				documentation = {
+					auto_show = true,
+					auto_show_delay_ms = 200,
+				},
+				ghost_text = {
+					enabled = true, -- pon true si quieres “texto fantasma”
+				},
+			},
 
-      sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
-      },
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+			},
 
-      cmdline = {
-        enabled = true,
-        keymap = {
-          preset = "cmdline",
-          ["<Right>"] = false,
-          ["<Left>"] = false,
-        },
-        completion = {
-          list = { selection = { preselect = false } },
-          menu = {
-            auto_show = function()
-              return vim.fn.getcmdtype() == ":"
-            end,
-          },
-          ghost_text = { enabled = true },
-        },
-      },
+			cmdline = {
+				enabled = true,
+				keymap = {
+					preset = "cmdline",
+					["<Right>"] = false,
+					["<Left>"] = false,
+				},
+				completion = {
+					list = { selection = { preselect = false } },
+					menu = {
+						auto_show = function()
+							return vim.fn.getcmdtype() == ":"
+						end,
+					},
+					ghost_text = { enabled = true },
+				},
+			},
 
-      keymap = {
-        preset = "enter",
-        ["<C-y>"] = { "select_and_accept" },
-      },
-    },
+			keymap = {
+				preset = "enter",
+				["<C-y>"] = { "select_and_accept" },
+			},
+		},
 
-    config = function(_, opts)
-      require("blink.cmp").setup(opts)
-    end,
-  },
+		config = function(_, opts)
+			require("blink.cmp").setup(opts)
+		end,
+	},
 }
--- lua/plugins/showkeys.lua
+++ lua/plugins/showkeys.lua
@@ -1,33 +1,81 @@
--- Muestra en pantalla las combinaciones de teclas presionadas en tiempo real.
--- Ideal para practicar, grabar o depurar tus atajos de Neovim.
 return {
 	"nvzone/showkeys",
-	lazy = false, -- cargar al inicio
+	lazy = false, -- lo cargamos al inicio para poder auto-activarlo
 	cmd = "ShowkeysToggle",
+
 	keys = {
-		{
-			"<leader>ks",
-			"<cmd>ShowkeysToggle<cr>",
-			desc = "Mostrar teclas presionadas (Showkeys)",
-		},
+		{ "<leader>ks", "<cmd>ShowkeysToggle<cr>", desc = "Mostrar teclas (Showkeys)" },
 	},
+
 	opts = {
-		timeout = 100, -- tiempo de espera para mostrar la combinación
-		maxkeys = 4,
-		position = "bottom-center", -- posición del recuadro
-		winopts = {
-			height = 1, -- altura del recuadro flotante
-		},
+		timeout = 100,
+		maxkeys = 5,
+		position = "bottom-center",
+		winopts = { height = 1 },
 	},
-	config = function()
-		require("showkeys").setup({
-			timeout = 100,
-			maxkeys = 5,
-			position = "bottom-center",
-			winopts = {
-				height = 1,
-			},
+
+	config = function(_, opts)
+		local showkeys = require("showkeys")
+		showkeys.setup(opts)
+
+		-- -----------------------------
+		-- Auto ON al entrar a Neovim
+		-- -----------------------------
+		vim.api.nvim_create_autocmd("VimEnter", {
+			callback = function()
+				pcall(vim.cmd, "ShowkeysToggle") -- lo enciende al inicio
+			end,
 		})
-		vim.cmd("ShowkeysToggle") -- activarlo automáticamente
+
+		-- -----------------------------
+		-- Auto OFF tras 5s sin actividad
+		-- -----------------------------
+		local timer = vim.loop.new_timer()
+		local HIDE_AFTER = 5000 -- ms
+		local enabled = true -- asumimos que al iniciar lo encendemos
+
+		local function stop_timer()
+			if timer and not timer:is_closing() then
+				timer:stop()
+			end
+		end
+
+		local function schedule_hide()
+			stop_timer()
+			timer:start(
+				HIDE_AFTER,
+				0,
+				vim.schedule_wrap(function()
+					if enabled then
+						enabled = false
+						pcall(vim.cmd, "ShowkeysToggle") -- apaga
+					end
+				end)
+			)
+		end
+
+		-- Cada “actividad” reinicia el timer y (si estaba apagado) lo vuelve a encender
+		local function on_activity()
+			if not enabled then
+				enabled = true
+				pcall(vim.cmd, "ShowkeysToggle") -- enciende cuando vuelves a teclear
+			end
+			schedule_hide()
+		end
+
+		vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI", "InsertCharPre", "CmdlineChanged" }, {
+			callback = on_activity,
+		})
+
+		-- Si tú lo toggleaseas manualmente con <leader>sk, mantenemos el estado coherente
+		vim.keymap.set("n", "<leader>sk", function()
+			enabled = not enabled
+			pcall(vim.cmd, "ShowkeysToggle")
+			if enabled then
+				schedule_hide()
+			else
+				stop_timer()
+			end
+		end, { desc = "Mostrar teclas (Showkeys)" })
 	end,
 }
